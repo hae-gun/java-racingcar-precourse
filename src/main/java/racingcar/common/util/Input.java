9dd7fb8ac4afe4054b8867b0cf6c80228ef82eef
Hae-gun <newchk610@gmail.com>
2022-10-08 15:28:53 +0900

    feat: util class 작성

---
+++ src/main/java/racingcar/common/util/Input.java
@@ -0,0 +1,11 @@
+package racingcar.common.util;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.io.IOException;
+
+public class Input {
+    public static String readLine() throws IOException {
+        return Console.readLine();
+    }
+}
---
+++ src/main/java/racingcar/common/util/Output.java
@@ -0,0 +1,11 @@
+package racingcar.common.util;
+
+public class Output {
+    public static void println(Object obj){
+        System.out.println(obj);
+    }
+
+    public static void print(Object obj){
+        System.out.println(obj);
+    }
+}
