0e4ccd4da1782a8a1d86517601a939028e1f2463
Hae-gun <newchk610@gmail.com>
2022-10-08 15:35:43 +0900

    refactor: 에러 메시지 추가

--- src/main/java/racingcar/common/exception/ErrorMessage.java
+++ src/main/java/racingcar/common/exception/ErrorMessage.java
@@ -2,7 +2,8 @@ package racingcar.common.exception;
 
 public enum ErrorMessage {
     ILLEGAL_INPUT_VALUE("잘못된 입력값입니다."),
-    CAN_NOT_NEGATIVE("음수를 입력할 수 없습니다.");
+    CAN_NOT_NEGATIVE("음수를 입력할 수 없습니다."),
+    NAME_LENGTH_ERROR("이름의 길이는 0이상 5이하만 가능 합니다.");
     private String message;
 
     ErrorMessage(String message) {
