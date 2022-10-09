fb0351bd6a9a2cada6e03e33bf71269a090ea694
Hae-gun <newchk610@gmail.com>
2022-10-09 19:48:38 +0900

    refactor: errorMessage prefix 추가 "[ERROR]"

--- src/main/java/racingcar/common/exception/ErrorMessage.java
+++ src/main/java/racingcar/common/exception/ErrorMessage.java
@@ -1,10 +1,10 @@
 package racingcar.common.exception;
 
 public enum ErrorMessage {
-    ILLEGAL_INPUT_VALUE("잘못된 입력값입니다."),
-    CAN_NOT_NEGATIVE("음수를 입력할 수 없습니다."),
-    NAME_LENGTH_ERROR("이름의 길이는 0이상 5이하만 가능 합니다."),
-    CAN_NOT_ADD_SAME_CAR("리스트에 동일한 Car가 존재합니다.");
+    ILLEGAL_INPUT_VALUE("[ERROR] 잘못된 입력값입니다."),
+    CAN_NOT_NEGATIVE("[ERROR] 음수를 입력할 수 없습니다."),
+    NAME_LENGTH_ERROR("[ERROR] 이름의 길이는 0이상 5이하만 가능 합니다."),
+    CAN_NOT_ADD_SAME_CAR("[ERROR] 리스트에 동일한 Car가 존재합니다.");
     private String message;
 
     ErrorMessage(String message) {
